5b9b4099738f133f6e5ae87f22c7ef1805431fc4
Victor Rojas <vicko16@gmail.com>
2022-05-24 17:50:42 -0500

    add ambassador http client and use it to validate the api key

--- login/login.js
+++ login/login.js
@@ -1,10 +1,23 @@
 const core = require('@actions/core')
 const exec = require('@actions/exec');
+const AmbassadorClient = require('../ambassador-client').AmbassadorClient;
 
 const telepresenceLogin = async function(){
+    const apiKey = core.getInput('telepresence_api_key');
+    if (!apiKey) {
+        code.error('telepresence_api_key is required')
+        return;
+    }
+
+    const ambassadorClient = new AmbassadorClient(apiKey);
+    const keyValid = ambassadorClient.isApiKeyValid();
+    if (!keyValid) {
+        core.error('telepresence_api_key is expired or invalid');
+        return;
+    }
+
     try {
-        const API_KEY = core.getInput('telepresence_api_key');
-        await exec.exec('telepresence', ['login', '--apikey', API_KEY]);
+        await exec.exec('telepresence', ['login', '--apikey', apiKey]);
     } catch (error) {
         core.setFailed(error.message);
     }
