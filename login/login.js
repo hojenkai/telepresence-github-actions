fbd883d1368018f35212bda71f81e99659986d40
Victor Rojas <vicko16@gmail.com>
2022-05-24 20:41:53 -0500

    set failed when validations fail

--- login/login.js
+++ login/login.js
@@ -5,14 +5,15 @@ const AmbassadorClient = require('../ambassador-client').AmbassadorClient;
 const telepresenceLogin = async function(){
     const apiKey = core.getInput('telepresence_api_key');
     if (!apiKey) {
-        core.error('telepresence_api_key is required')
+        core.setFailed('telepresence_api_key is required');
         return;
     }
 
     const ambassadorClient = new AmbassadorClient(apiKey);
     const keyValid = ambassadorClient.isApiKeyValid();
+    core.notice("validation result " + keyValid);
     if (!keyValid) {
-        core.error('telepresence_api_key is expired or invalid');
+        core.setFailed('telepresence_api_key is expired or invalid');
         return;
     }
 
