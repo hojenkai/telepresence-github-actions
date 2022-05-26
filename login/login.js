b7a9d452ec92b36e090ec9f3a4b3d6776e9a107e
Victor Rojas <vicko16@gmail.com>
2022-05-26 10:48:07 -0500

    remove console logs

--- login/login.js
+++ login/login.js
@@ -4,7 +4,6 @@ const AmbassadorClient = require('../ambassador/api-client');
 
 const telepresenceLogin = async function(){
     const apiKey = core.getInput('telepresence_api_key');
-    console.log('apikey', apiKey);
     if (!apiKey) {
         core.setFailed('telepresence_api_key is required');
         return;
@@ -13,8 +12,6 @@ const telepresenceLogin = async function(){
     const ambassadorClient = new AmbassadorClient(apiKey);
     const keyValid = await ambassadorClient.isApiKeyValid();
     if (!keyValid) {
-        console.log('invalid key',apiKey, core.setFailed)
-        // core.setFailed('telepresence_api_key is expired or invalid');
         core.setFailed('telepresence_api_key is expired or invalid');
         return;
     }
